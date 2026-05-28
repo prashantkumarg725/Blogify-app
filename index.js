e534ea80a5efb23fb74a807c04cb5e07cfc1c491
Prashant <prashantkumarg725@gmail.com>
2026-05-28 10:46:56 +0530

    restored full app

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
 
+// Routes
+const blogRoutes = require("./routes/blog");
+const userRoutes = require("./routes/user");
+
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// View Engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Routes
+app.use("/", blogRoutes);
+app.use("/user", userRoutes);
+
+// Home fallback
 app.get("/", (req, res) => {
-  res.send("Blogify Backend Running");
+  res.render("home");
 });
 
 // Port
